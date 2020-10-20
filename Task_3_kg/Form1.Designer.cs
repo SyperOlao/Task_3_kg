5baea317d36d6f71ad98dbd659955be4e1d1296d
Syper_olao <olay1234538@gmail.com>
2020-10-21 01:26:37 +0300

    Добавьте файлы проекта.

---
+++ Task_3_kg/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Task_3_kg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textboxAngle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(579, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(614, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите надпись";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(617, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "павррапрап";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(613, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Коэффициент увеличения";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(642, 98);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(49, 22);
+            this.textBoxX.TabIndex = 4;
+            this.textBoxX.Text = "150";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(614, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Угол наклона";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(739, 98);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(49, 22);
+            this.textBoxY.TabIndex = 6;
+            this.textBoxY.Text = "70";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(642, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(616, 227);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(172, 22);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "1";
+            // 
+            // textboxAngle
+            // 
+            this.textboxAngle.Location = new System.Drawing.Point(616, 272);
+            this.textboxAngle.Name = "textboxAngle";
+            this.textboxAngle.Size = new System.Drawing.Size(172, 22);
+            this.textboxAngle.TabIndex = 9;
+            this.textboxAngle.Text = "90";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(615, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "X:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(714, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Y:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textboxAngle);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "task_3_7";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textboxAngle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Task_3_kg/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Написать процедуру, выводящую строку символов(с коэффициентом увеличения) под любым углом.
+namespace Task_3_kg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            string userSTR = textBox1.Text;
+            int x = int.Parse(textBoxX.Text);
+            int y = int.Parse(textBoxY.Text);
+            int alfa = int.Parse(textboxAngle.Text);
+            float size = float.Parse(textBox4.Text.Replace(".", ","));
+            graphics.RotateTransform(alfa);
+            graphics.TranslateTransform(150, 50);
+            double Xnew = x * Math.Cos(alfa) + y * Math.Sin(alfa);
+            double Ynew = -x * Math.Sin(alfa) + y * Math.Cos(alfa);
+            if (alfa >= 0 && alfa <= 90)
+            {
+                graphics.DrawString(userSTR, new Font("Arial", size * 15.0f), new SolidBrush(Color.Black), y + (float)Ynew, -x - (float)Xnew);
+            }
+            if (alfa > 90 && alfa <= 180)
+            {
+                graphics.DrawString(userSTR, new Font("Arial", size * 15.0f), new SolidBrush(Color.Black), -x - (float)Xnew, -y - (float)Ynew);
+            }
+          
+            graphics.ResetTransform();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = CreateGraphics();
+            Draw(graphics);
+            pictureBox1.Refresh(); 
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Draw(graphics);
+        }
+
+
+    }
+}
